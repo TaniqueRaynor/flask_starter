cd99e7777947a030d7ed10cc96a88cbc5a0c31ec
Tanique Raynor <tanique.raynor@gmail.com>
2022-03-22 21:42:59 -0500

    Project1

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'Som3$ec5etK*y'
 app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://hztvetvygllwog:8c7c80a849b15c6923823e0e0e0b9027db3defce93ae77d1619ffca14a37302d@ec2-3-225-213-67.compute-1.amazonaws.com:5432/d9t92ddtmou907'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://hztvetvygllwog:8c7c80a849b15c6923823e0e0e0b9027db3defce93ae77d1619ffca14a37302d@ec2-3-225-213-67.compute-1.amazonaws.com:5432/d9t92ddtmou907'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db = SQLAlchemy(app)
